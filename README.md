e9126abd80dfa9c3390d6ea37903ca17cc8fdf2b
tomo <m.tomo0912.tm@gmail.com>
2022-05-21 09:21:11 +0900

    資産整理

--- README.md
+++ README.md
@@ -15,3 +15,24 @@
 ```
 # docker-compose up
 ```
+
+### 履歴
+
+#### 2022年05月21日　ver 0.4
+githubに公開
+Markdownデータを複数管理
+複数データ取得～取得したデータを画面に表示
+
+#### 2022年05月20日　ver 0.3
+React-MUIで部品化
+React-MUIでUIテンプレート化
+React-route-dom利用
+React-mui利用
+ReactからFlask経由でMariaDBにデータを取得する
+ReactからFlask経由でMariaDBにデータを登録する
+
+#### 2022年05月19日　ver 0.2
+react-markdownとremark-gfm機能追加
+
+#### 2022年05月18日　ver 0.1
+環境構築
--- db/init/init.sql
+++ db/init/init.sql
@@ -1,8 +1,14 @@
-CREATE DATABASE IF NOT EXISTS test;
+-- 疎通確認用
+-- CREATE DATABASE IF NOT EXISTS test;
+-- CREATE TABLE IF NOT EXISTS test.user (id int, name varchar(10));
+-- REPLACE INTO test.user  VALUES (1, 'TAROU');
 
-CREATE TABLE IF NOT EXISTS test.user (id int, name varchar(10));
+-- Markdownアプリ初期確認用
+-- CREATE TABLE IF NOT EXISTS test.markdown (body text);
 
-REPLACE INTO test.user  VALUES (1, 'TAROU');
+-- Markdown複数管理用 (Markdownアプリ)
 
+CREATE TABLE IF NOT EXISTS test.markdown (url varchar(500), title varchar(250), body text);
 
-CREATE TABLE IF NOT EXISTS test.markdown (body text);
+REPLACE INTO test.markdown (url, title, body) VALUES ('test/test', 'test', '# TEST');
+REPLACE INTO test.markdown (url, title, body) VALUES ('test/test2', 'test2', '# TEST2');
